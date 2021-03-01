96bd6d48b14bff72a3f2f14342c0e872a40e988b
inck <kui_cui@ziguangcn.com>
2021-03-01 19:27:07 +0800

    添加 翻译npm 添加翻译处理

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import Koa from 'koa';
-import http from 'http';
+import { baidu } from 'translation.js';
 import Router from 'koa-router';
 import { readFileSync, readdirSync } from 'fs';
 import { resolve } from 'path';
-import translation from './utils/translation'
 // 使用 import 需要 npm install babel-cli  -D npm install babel-preset-env -D
 // 启动命令添加环境 babel-node --presets env app.js
 // npm install -g nodemon 
@@ -11,6 +10,9 @@ import translation from './utils/translation'
 
 const app = new Koa();
 const router = new Router;
+// const translation = new Google();
+// const trans = translation.create('Google')
+
 
 router.get('/', async (ctx, next) => {
   await next();
@@ -26,6 +28,7 @@ router.get('/', async (ctx, next) => {
   const worldCN = await readFileSync(resolve(__dirname, '../../react/public/echarts-map-data-master/world/world-mapping.json'));
 
   // console.log(path)
+  
 
   let names = new Function(`return ${world.toString()}`)();
   names = names.features.map(i => i.properties.name)
@@ -34,8 +37,15 @@ router.get('/', async (ctx, next) => {
   let data = JSON.parse(JSON.stringify(worldCNMap));
   worldCNMap = Object.keys(worldCNMap);
 
-  // let f = await translation('file');
-
+  let translate = (text) => {
+    return baidu.translate({
+      from: 'en',
+      to: 'zh-CN',
+      text,
+    }).then(res => ({
+      [text]: res.result
+    }));
+  }
 
   let NotCnMap = names.filter(i => !worldCNMap.includes(i));
 
@@ -45,16 +55,15 @@ router.get('/', async (ctx, next) => {
 
   NotJson.push(...NotJson.map(i => data[i]))
 
-  console.log(
-    // await translation('Bosnia and Herzegovina')
-  )
+  console.log(await Promise.all(NotCnMap.map(async i => await translate(i))))
 
   ctx.body = res.toString()
     .replace(/'<%world%>'/g, world.toString())
     .replace(/'<%file%>'/g, JSON.stringify(file))
     .replace(/'<%worldCN%>'/g, worldCN.toString())
     .replace(/'<%NotCnMap%>'/g, JSON.stringify(NotCnMap))
-    .replace(/'<%NotJson%>'/g, JSON.stringify(NotJson));
+    .replace(/'<%NotJson%>'/g, JSON.stringify(NotJson))
+    // .replace(/'<%T%>'/g, JSON.stringify(t));
 });
 
 
